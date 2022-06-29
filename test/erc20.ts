feee4ca2d1e816b34a08928365d005baf3c03b51
Harris <acyclicgraphs@gmail.com>
2022-06-29 14:30:47 -0400

    Update erc20.ts

--- test/erc20.ts
+++ test/erc20.ts
@@ -64,6 +64,7 @@ describe("erc20", function () {
       // TODO: check the Approval Event is emitted
 
       // WIP
+      // @info check this article for reference
 
       // const tokenAmount: number = 37;
       // const txResult1 = await token.connect(signers[0].address).approve(signers[0].address, tokenAmount);
