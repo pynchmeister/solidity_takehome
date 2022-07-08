e170350f95bd384660b900c5223ccdef3b200838
Harris Levine <acyclicgraphs@gmail.com>
2022-07-08 10:21:22 -0400

    Grant funding contract testing

--- test/grantfunding.ts
+++ test/grantfunding.ts
@@ -3,9 +3,31 @@ import { ethers, waffle } from "hardhat";
 // import { ERC20 } from "../typechain/ERC20";
 // import { ERC20__factory } from "../typechain/factories/ERC20__factory";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/dist/src/signer-with-address";
+import { ERC20 } from "../typechain/ERC20";
+import { GrantFunding } from "../typechain/GrantFunding";
 
 const { provider } = waffle;
 
 describe("GrantFunding", function () {
 
+    let token: ERC20;
+    let grantFunding: GrantFunding;
+    const [wallet] = provider.getWallets();
+    let signers: SignerWithAddress[];
+    
+    before(async function () {
+        signers = await ethers.getSigners();
+    });
+
+    it("creates a new grant successfully", async () => {
+
+    });
+
+    it("removes grant", async () => {
+
+    });
+
+    it("claims grant successfully", async () => {
+
+    });
 });
