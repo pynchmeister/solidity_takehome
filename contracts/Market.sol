aa73ceb155e13b3eb3c0677ae760dc44be3ae61f
Harris Levine <acyclicgraphs@gmail.com>
2022-07-07 21:04:05 -0400

    Add Slither security vulnerabilities

--- contracts/Market.sol
+++ contracts/Market.sol
@@ -99,3 +99,78 @@ contract Market is ERC20 {
         payable(msg.sender).transfer(address(this).balance);
     }
 }
+
+/*
+ Security Vulernabilities: Market.sol
+
+ (Using Slither)
+
+ * Market.deposit(address,address,uint256) (contracts/Market.sol#17-32) uses delegatecall to a input-controlled function id
+        - (success) = pool.delegatecall(abi.encodeWithSignature(tokenDeposit(address, address, uint256),msg.sender,token,amount)) (contracts/Market.sol#26-30)
+ * Market.withdraw(uint256,address,address,uint256) (contracts/Market.sol#34-46) uses delegatecall to a input-controlled function id
+        - (success) = pool.delegatecall(abi.encodeWithSignature(withdraw(address, address),msg.sender,token)) (contracts/Market.sol#36-37)
+Reference: https://github.com/crytic/slither/wiki/Detector-Documentation#controlled-delegatecall
+
+
+ * Market.deposit(address,address,uint256) (contracts/Market.sol#17-32) ignores return value by ERC20(token).transferFrom(msg.sender,address(this),amount) (contracts/Market.sol#24)
+ * Market.withdraw(uint256,address,address,uint256) (contracts/Market.sol#34-46) ignores return value by ERC20(token).transfer(msg.sender,amount) (contracts/Market.sol#39)
+Reference: https://github.com/crytic/slither/wiki/Detector-Documentation#unchecked-transfer
+
+ * Market.usedSignatures (contracts/Market.sol#11) is never initialized. It is used in:
+        - Market.signedTransfer(address,address,uint256,bytes32,bytes32,bytes32,uint8) (contracts/Market.sol#49-55)
+Reference: https://github.com/crytic/slither/wiki/Detector-Documentation#uninitialized-state-variables
+//
+
+ * Market.deposit(address,address,uint256).token (contracts/Market.sol#17) lacks a zero-check on :
+                - (success) = pool.delegatecall(abi.encodeWithSignature(tokenDeposit(address, address, uint256),msg.sender,token,amount)) (contracts/Market.sol#26-30)
+ * Market.withdraw(uint256,address,address,uint256).pool (contracts/Market.sol#34) lacks a zero-check on :
+                - (success) = pool.delegatecall(abi.encodeWithSignature(withdraw(address, address),msg.sender,token)) (contracts/Market.sol#36-37)
+ * Market.withdraw(uint256,address,address,uint256).token (contracts/Market.sol#34) lacks a zero-check on :
+                - (success) = pool.delegatecall(abi.encodeWithSignature(withdraw(address, address),msg.sender,token)) (contracts/Market.sol#36-37)
+Reference: https://github.com/crytic/slither/wiki/Detector-Documentation#missing-zero-address-validation
+
+* Reentrancy in Market.withdraw(uint256,address,address,uint256) (contracts/Market.sol#34-46):
+        External calls:
+        - (success) = pool.delegatecall(abi.encodeWithSignature(withdraw(address, address),msg.sender,token)) (contracts/Market.sol#36-37)
+        - ERC20(token).transfer(msg.sender,amount) (contracts/Market.sol#39)
+        State variables written after the call(s):
+        - _burn(msg.sender,lpTokens) (contracts/Market.sol#44)
+                - balanceOf[account] = balanceOf[account] - amount (contracts/ERC20.sol#128)
+        - _burn(msg.sender,lpTokens) (contracts/Market.sol#44)
+                - totalSupply -= amount (contracts/ERC20.sol#130)
+Reference: https://github.com/crytic/slither/wiki/Detector-Documentation#reentrancy-vulnerabilities-2
+
+* Reentrancy in Market.withdraw(uint256,address,address,uint256) (contracts/Market.sol#34-46):
+        External calls:
+        - (success) = pool.delegatecall(abi.encodeWithSignature(withdraw(address, address),msg.sender,token)) (contracts/Market.sol#36-37)
+        - ERC20(token).transfer(msg.sender,amount) (contracts/Market.sol#39)
+        Event emitted after the call(s):
+        - Transfer(account,address(0),amount) (contracts/ERC20.sol#132)
+                - _burn(msg.sender,lpTokens) (contracts/Market.sol#44)
+Reference: https://github.com/crytic/slither/wiki/Detector-Documentation#reentrancy-vulnerabilities-3
+
+* Market.registerPool(address) (contracts/Market.sol#64-96) uses assembly
+        - INLINE ASM (contracts/Market.sol#70-82)
+Reference: https://github.com/crytic/slither/wiki/Detector-Documentation#assembly-usage
+
+ERC20._setupDecimals(uint8) (contracts/ERC20.sol#154-157) is never used and should be removed
+Reference: https://github.com/crytic/slither/wiki/Detector-Documentation#dead-code
+
+Pragma version^0.8.0 (contracts/ERC20.sol#3) allows old versions
+Pragma version^0.8.0 (contracts/Market.sol#1) allows old versions
+Reference: https://github.com/crytic/slither/wiki/Detector-Documentation#incorrect-versions-of-solidity
+
+* Low level call in Market.deposit(address,address,uint256) (contracts/Market.sol#17-32):
+        - (success) = pool.delegatecall(abi.encodeWithSignature(tokenDeposit(address, address, uint256),msg.sender,token,amount)) (contracts/Market.sol#26-30)
+Low level call in Market.withdraw(uint256,address,address,uint256) (contracts/Market.sol#34-46):
+        - (success) = pool.delegatecall(abi.encodeWithSignature(withdraw(address, address),msg.sender,token)) (contracts/Market.sol#36-37)
+Reference: https://github.com/crytic/slither/wiki/Detector-Documentation#low-level-calls
+
+* transfer(address,uint256) should be declared external:
+        - ERC20.transfer(address,uint256) (contracts/ERC20.sol#56-63)
+mint(address,uint256) should be declared external:
+        - ERC20.mint(address,uint256) (contracts/ERC20.sol#106-108)
+approve(address,uint256) should be declared external:
+        - ERC20.approve(address,uint256) (contracts/ERC20.sol#140-150)
+Reference: https://github.com/crytic/slither/wiki/Detector-Documentation#public-function-that-could-be-declared-external
+*/
